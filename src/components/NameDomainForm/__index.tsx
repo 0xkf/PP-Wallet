f9c3fe2f37dad746a5627711cde7cf25b9235356
0xkf <Kanichi73@me.com>
2023-06-19 16:55:37 +0900

    backup

--- src/components/NameDomainForm/__index.tsx
+++ src/components/NameDomainForm/__index.tsx
@@ -29,6 +29,24 @@ export const NameDomainForm: React.FC<NameDomainFormProp> = memo(
 
     const domain = `${twitterId.slice(1)}.ppw`;
 
+    // const handleClick = async () => {
+    //   const { contract, web3 } = makeNameDomainContract();
+    //   const fee = await contract.methods.price(domain).call();
+    //   console.log(fee);
+    //   const myAddress = await web3.eth.getAccounts();
+    //   console.log(myAddress);
+    //   //TODO: polygon gas station
+    //   await contract.methods.register(domain).send({
+    //     from: myAddress[0],
+    //     value: fee,
+    //     maxPriorityFeePerGas: web3.utils.toWei("3", "gwei"),
+    //     maxFeePerGas: web3.utils.toWei("3", "gwei"),
+    //   });
+    //   setStep(3);
+    //   return "resolved";
+    // };
+    // take note that same id cannot be minted
+    
     const handleClick = async () => {
         try {
           const { contract, web3 } = makeNameDomainContract();
@@ -82,6 +100,35 @@ export const NameDomainForm: React.FC<NameDomainFormProp> = memo(
       }
     };
 
+    // return (
+    //   <div className="flex flex-col justify-center gap-y-6 items-center">
+    //     <div className="flex flex-col items-center p-4 border-2 border-success rounded-md">
+    //       <div className="text-xl">
+    //         address: <span className="text-success">{address}</span>
+    //       </div>
+    //       <div>
+    //         <a
+    //           href={`https://mumbai.polygonscan.com/address/${address}`}
+    //           className="link"
+    //         >
+    //           Visit Polygon Scan
+    //         </a>
+    //       </div>
+    //     </div>
+    //     <button className="btn btn-primary" onClick={() => connectMetamask()}>
+    //       Connect Wallet
+    //     </button>
+    //     <button className="btn btn-secondary" onClick={() => sendTransaction()}>
+    //       Send 0.01MATIC Chip
+    //     </button>
+    //     <div className="flex justify-center gap-x-6 items-center">
+    //       <div className="text-accent text-xl underline">{domain}</div>
+    //       <button className="btn btn-primary" onClick={() => handleClick()}>
+    //         Register Domain
+    //       </button>
+    //     </div>
+    //   </div>
+    // );
     return (
         <div className="flex flex-col justify-center gap-y-6 items-center">
           <div className="flex flex-col items-center p-4 border-2 border-success rounded-md">
